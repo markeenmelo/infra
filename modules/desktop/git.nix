e3e0f3c1d0deff5ed61ebaf68e741250efb1f3e1
Marcos Melo <8158196+markeenmelo@users.noreply.github.com>
2026-09-14 16:18:28 -0400

    Update desktop configuration and dependencies
    * update packages
    
    * setup git and github cli
    
    * set monitors to higher res
    
    * Update SKILL.md
    
    * add extension so i can reser codex quotas

--- modules/desktop/git.nix
+++ modules/desktop/git.nix
@@ -1,5 +1,11 @@
 {
-  flake.modules.homeManager.desktop.programs.git.enable = true;
+  flake.modules.homeManager.desktop.programs = {
+    git.enable = true;
+    gh = {
+      enable = true;
+      gitCredentialHelper.enable = true;
+    };
+  };
 
   fleet.hosts.thinkpad.module.home-manager.users.marcos.programs.git.settings.user = {
     name = "Marcos Melo";
