757fa242f7f2b2f5971ace5af47cc9aa8ee5998f
Marcos Melo <marcosmelo@proton.me>
2026-09-15 11:10:43 -0400

    Add UGREEN disk activity LED support

--- modules/hosts/bastion/hardware.nix
+++ modules/hosts/bastion/hardware.nix
@@ -1,33 +1,134 @@
 {
-  fleet.hosts.bastion.module = { lib, pkgs, ... }: {
-    system.stateVersion = "26.05";
-    boot.initrd.availableKernelModules = [
-      "xhci_pci"
-      "nvme"
-      "ahci"
-      "sd_mod"
-    ];
-    boot.kernelModules = [
-      "i2c-i801"
-      "i2c-dev"
-    ];
-    systemd.services.ugreen-leds = {
-      description = "Initialize UGREEN front-panel LEDs";
-      wantedBy = [ "multi-user.target" ];
-      requires = [ "systemd-modules-load.service" ];
-      after = [ "systemd-modules-load.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        TimeoutStartSec = "15s";
-        ExecStart = [
-          "${lib.getExe pkgs.ugreen-leds-cli} power netdev disk1 disk2 disk3 disk4 -off"
-          "${lib.getExe pkgs.ugreen-leds-cli} power -color 0 0 255 -brightness 128 -on"
-        ];
+  fleet.hosts.bastion.module =
+    {
+      config,
+      lib,
+      pkgs,
+      utils,
+      ...
+    }:
+    let
+      inherit (config.boot.kernelPackages) kernel kernelModuleMakeFlags;
+      inherit (pkgs.ugreen-leds-cli) src version;
+      diskLeds = {
+        disk1 = "ata-ST4000VN006-3CW104_ZW63V4FM";
+        disk2 = "ata-TOSHIBA_HDWG440_2270A00MFZ1G";
       };
+      ledDriver = pkgs.stdenv.mkDerivation {
+        pname = "led-ugreen";
+        version = "${version}-${kernel.version}";
+        inherit src;
+        sourceRoot = "${src.name}/kmod";
+        nativeBuildInputs = kernel.moduleBuildDependencies;
+        hardeningDisable = [ "pic" ];
+        makeFlags = kernelModuleMakeFlags ++ [
+          "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+        ];
+        installPhase = ''
+          runHook preInstall
+          install -Dm644 led-ugreen.ko $out/lib/modules/${kernel.modDirVersion}/extra/led-ugreen.ko
+          runHook postInstall
+        '';
+      };
+      diskMonitor = pkgs.runCommandCC "ugreen-blink-disk-${version}" { } ''
+        mkdir -p $out/bin
+        $CXX -std=c++17 -O2 ${src}/scripts/blink-disk.cpp -o $out/bin/ugreen-blink-disk
+      '';
+    in
+    {
+      system.stateVersion = "26.05";
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "ahci"
+        "sd_mod"
+      ];
+      boot.extraModulePackages = [ ledDriver ];
+      boot.kernelModules = [
+        "i2c-i801"
+        "i2c-dev"
+        "led-ugreen"
+        "ledtrig-netdev"
+        "ledtrig-oneshot"
+      ];
+      systemd.services = {
+        ugreen-leds = {
+          description = "Initialize UGREEN front-panel LEDs";
+          wantedBy = [ "multi-user.target" ];
+          requires = [ "systemd-modules-load.service" ];
+          after = [ "systemd-modules-load.service" ];
+          path = [
+            pkgs.kmod
+            pkgs.i2c-tools
+          ];
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            TimeoutStartSec = "15s";
+          };
+          script = ''
+            ${pkgs.bash}/bin/bash ${src}/scripts/ugreen-probe-leds
+            for led in power netdev disk1 disk2 disk3 disk4; do
+              echo none > /sys/class/leds/$led/trigger
+              echo 0 > /sys/class/leds/$led/brightness
+              echo '0 0 255' > /sys/class/leds/$led/color
+            done
+            echo 128 > /sys/class/leds/power/brightness
+            echo 128 > /sys/class/leds/netdev/brightness
+            echo netdev > /sys/class/leds/netdev/trigger
+            echo enp3s0 > /sys/class/leds/netdev/device_name
+            echo 100 > /sys/class/leds/netdev/interval
+            echo 1 > /sys/class/leds/netdev/link
+            echo 1 > /sys/class/leds/netdev/tx
+            echo 1 > /sys/class/leds/netdev/rx
+          '';
+        };
+      }
+      // lib.mapAttrs' (
+        led: id:
+        let
+          device = "/dev/disk/by-id/${id}";
+          deviceUnit = "${utils.escapeSystemdPath device}.device";
+          ledPath = "/sys/class/leds/${led}";
+        in
+        lib.nameValuePair "ugreen-${led}" {
+          description = "UGREEN ${led} presence and activity";
+          wantedBy = [ deviceUnit ];
+          bindsTo = [ deviceUnit ];
+          requires = [ "ugreen-leds.service" ];
+          after = [
+            deviceUnit
+            "ugreen-leds.service"
+          ];
+          partOf = [ "ugreen-leds.service" ];
+          preStart = ''
+            echo oneshot > ${ledPath}/trigger
+            echo 100 > ${ledPath}/delay_on
+            echo 100 > ${ledPath}/delay_off
+            echo 1 > ${ledPath}/invert
+            echo 128 > ${ledPath}/brightness
+          '';
+          script = ''
+            device=$(readlink -e ${lib.escapeShellArg device})
+            exec ${diskMonitor}/bin/ugreen-blink-disk 0.1 "''${device##*/}" ${led}
+          '';
+          postStop = ''
+            echo 0 > ${ledPath}/brightness
+          '';
+          serviceConfig = {
+            Restart = "on-failure";
+            RestartSec = "2s";
+            NoNewPrivileges = true;
+            PrivateNetwork = true;
+            ProtectHome = true;
+            ProtectSystem = "strict";
+            ReadWritePaths = [ ledPath ];
+            DevicePolicy = "closed";
+          };
+        }
+      ) diskLeds;
+      hardware.enableRedistributableFirmware = true;
+      hardware.cpu.intel.updateMicrocode = true;
+      boot.loader.limine.extraConfig = "graphics: no";
     };
-    hardware.enableRedistributableFirmware = true;
-    hardware.cpu.intel.updateMicrocode = true;
-    boot.loader.limine.extraConfig = "graphics: no";
-  };
 }
