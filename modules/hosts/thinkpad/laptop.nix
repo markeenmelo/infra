de240003a7b719c21a5c293a4ccfc2d393a8551b
Marcos Melo <marcosmelo@proton.me>
2026-09-15 12:09:32 -0400

    Restore native laptop power management

--- modules/hosts/thinkpad/laptop.nix
+++ modules/hosts/thinkpad/laptop.nix
@@ -1,10 +1,11 @@
 {
   fleet.hosts.thinkpad.module = {
-    services.logind.settings.Login = {
-      HandleLidSwitch = "suspend";
-      HandleLidSwitchExternalPower = "suspend";
-      HandleLidSwitchDocked = "ignore";
-    };
+    services.thermald.enable = true;
+    services.udev.extraHwdb = ''
+      battery:BAT0:*:dmi:*
+       CHARGE_LIMIT=85,90
+    '';
+    systemd.services.upower.preStart = "printf 1 > /var/lib/upower/charging-threshold-status";
     services.hardware.bolt.enable = true;
     environment.persistence."/persist".directories = [ "/var/lib/boltd" ];
   };
--- modules/laptop.nix
+++ modules/laptop.nix
@@ -1,15 +1,11 @@
 {
   flake.modules.nixos.laptop = {
     services.upower.enable = true;
-    services.tlp = {
-      enable = true;
-      pd.enable = true;
-      settings = {
-        TLP_AUTO_SWITCH = 1;
-        TLP_PROFILE_AC = "BAL";
-        TLP_PROFILE_BAT = "SAV";
-      };
-    };
-    environment.persistence."/persist".directories = [ "/var/lib/systemd/backlight" ];
+    services.power-profiles-daemon.enable = true;
+    environment.persistence."/persist".directories = [
+      "/var/lib/power-profiles-daemon"
+      "/var/lib/systemd/backlight"
+      "/var/lib/systemd/rfkill"
+    ];
   };
 }
