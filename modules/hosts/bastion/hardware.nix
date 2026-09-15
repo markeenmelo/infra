94be53fcb2faee4adf0655627f60c72a3c09b4e5
Marcos Melo <marcosmelo@proton.me>
2026-09-15 10:50:56 -0400

    Initialize Bastion LEDs at boot

--- modules/hosts/bastion/hardware.nix
+++ modules/hosts/bastion/hardware.nix
@@ -1,5 +1,5 @@
 {
-  fleet.hosts.bastion.module = {
+  fleet.hosts.bastion.module = { lib, pkgs, ... }: {
     system.stateVersion = "26.05";
     boot.initrd.availableKernelModules = [
       "xhci_pci"
@@ -7,6 +7,25 @@
       "ahci"
       "sd_mod"
     ];
+    boot.kernelModules = [
+      "i2c-i801"
+      "i2c-dev"
+    ];
+    systemd.services.ugreen-leds = {
+      description = "Initialize UGREEN front-panel LEDs";
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "systemd-modules-load.service" ];
+      after = [ "systemd-modules-load.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        TimeoutStartSec = "15s";
+        ExecStart = [
+          "${lib.getExe pkgs.ugreen-leds-cli} power netdev disk1 disk2 disk3 disk4 -off"
+          "${lib.getExe pkgs.ugreen-leds-cli} power -color 0 0 255 -brightness 128 -on"
+        ];
+      };
+    };
     hardware.enableRedistributableFirmware = true;
     hardware.cpu.intel.updateMicrocode = true;
     boot.loader.limine.extraConfig = "graphics: no";
